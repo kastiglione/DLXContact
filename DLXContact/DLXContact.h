4b3b0da330e6a6823d24d2f7f38e4bcbfac69ad8
Dave Lee <davelee.com@gmail.com>
2012-04-17 23:40:22 -0600

    Remove copyright comment

--- DLXContact/DLXContact.h
+++ DLXContact/DLXContact.h
@@ -3,7 +3,6 @@
 //  DLXContact
 //
 //  Created by Dave Lee on 12-04-17.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- DLXContactTests/DLXContactTests.m
+++ DLXContactTests/DLXContactTests.m
@@ -3,7 +3,6 @@
 //  DLXContactTests
 //
 //  Created by Dave Lee on 12-04-17.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import "DLXContactTests.h"
